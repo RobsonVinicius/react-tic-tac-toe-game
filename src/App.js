35f334aee7f0783521c61f7c91ce884aead21b5d
Robson Vinicius <robsondgn@outlook.com>
2019-06-04 15:24:00 -0300

    Feito componente botão Próximo Jogador

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import BoardCollab from "./components/BoardCollab";
 import SquareCollab from "./components/SquareCollab";
+import NextPlayer from "./components/NextPlayer";
 
 
 const App = () => ( 
   <>
+    <NextPlayer />
     <BoardCollab>    
       <SquareCollab />     
       <SquareCollab />     
