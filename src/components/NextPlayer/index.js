06868d171bdbe91ae8c1b387c8a4e8782678d7a1
Robson Vinicius <robsondgn@outlook.com>
2020-07-15 15:27:04 -0300

    Gerando nova base para construir um jogo novo

--- src/components/NextPlayer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import "./styles.css";
-
-const NextPlayer = ({player}) => (
-    <p className="next-player">Próximo jogador: {player}</p>  
-  );
-
-export default NextPlayer;
